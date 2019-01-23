ce6ba7569923dc974d92fb164248f1271756fa36
David Jirovec <david.jirovec@gmail.com>
2019-01-23 09:40:49 +0100

    Ugly and slow number formatting same as in js via nashorn

--- xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
+++ xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
@@ -1,17 +1,31 @@
 package info.bitrich.xchangestream.bitfinex;
 
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.StringWriter;
 import java.math.BigDecimal;
 
-public class ChecksumBigDecimalToStringConverter {
+class ChecksumBigDecimalToStringConverter {
 
-    public String convert(final BigDecimal bigDecimal) {
-        return bigDecimal.toString()
-                .toLowerCase()
-                .replace("+", "")
-                .replaceFirst("(.+\\.[^0]*)0+(e.*|$)", "$1$2")
-                .replaceFirst("\\.e", "e")
-                .replaceFirst("\\.$", "")
-                .replaceFirst("(.*\\..*[^0])0+$", "$1");
+    private static final ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
+
+    String convert(final BigDecimal bigDecimal) {
+        ScriptContext context = engine.getContext();
+        StringWriter writer = new StringWriter();
+        context.setWriter(writer);
+
+        try {
+            engine.eval("print(" + bigDecimal.toString() + ")");
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
+
+        return StringUtils.deleteWhitespace(writer.toString());
     }
 
 }
