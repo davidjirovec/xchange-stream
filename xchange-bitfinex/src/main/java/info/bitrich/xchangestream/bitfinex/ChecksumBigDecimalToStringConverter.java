325f9179b18ad5fd1d8d2fac02aa9fc2dd3f3446
David Jirovec <david.jirovec@gmail.com>
2019-02-22 18:38:52 +0100

    Improve performance

--- xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
+++ xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
@@ -1,32 +1,22 @@
 package info.bitrich.xchangestream.bitfinex;
 
 
-import org.apache.commons.lang3.StringUtils;
-
-import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class ChecksumBigDecimalToStringConverter {
 
-    String convert(final List<BigDecimal> bigDecimals) {
-        final ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
-        ScriptContext context = engine.getContext();
-        StringWriter writer = new StringWriter();
-        context.setWriter(writer);
+    private final static ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 
+    String convert(final List<BigDecimal> bigDecimals) {
         try {
-            engine.eval("print([" + bigDecimals.stream().map(BigDecimal::toString).collect(Collectors.joining(",")) + "].join(':'))");
+            return ((String) engine.eval(bigDecimals + ".join(':')"));
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
-
-        return StringUtils.deleteWhitespace(writer.toString());
     }
 
 }
