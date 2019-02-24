b0e8d1943b2e1dd276d2c71418ab71b3733b260e
David Jirovec <david.jirovec@gmail.com>
2019-02-24 18:07:57 +0100

    Use compiled script

--- xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
+++ xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/ChecksumBigDecimalToStringConverter.java
@@ -1,20 +1,43 @@
 package info.bitrich.xchangestream.bitfinex;
 
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 class ChecksumBigDecimalToStringConverter {
 
-    private final static ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    private static final ScriptEngine SCRIPT_ENGINE = new ScriptEngineManager().getEngineByName("nashorn");
+
+    static {
+        try {
+            ((Compilable) SCRIPT_ENGINE)
+                    .compile(
+                            new InputStreamReader(
+                                    Objects.requireNonNull(
+                                            ChecksumBigDecimalToStringConverter.class
+                                                    .getClassLoader()
+                                                    .getResourceAsStream("format.js")
+                                    )
+                            )
+                    )
+                    .eval();
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     String convert(final List<BigDecimal> bigDecimals) {
         try {
-            return ((String) engine.eval(bigDecimals + ".join(':')"));
-        } catch (ScriptException e) {
+            return (String) ((Invocable) SCRIPT_ENGINE)
+                    .invokeFunction(
+                            "format",
+                            bigDecimals.stream().map(BigDecimal::doubleValue).collect(Collectors.toList())
+                    );
+        } catch (ScriptException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
     }
